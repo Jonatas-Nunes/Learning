01d70f8567131a6de9b033b052ec34ea4e358dfa
Jonatas Nunes <jonatasnpe@hotmail.com>
2023-10-08 17:02:44 -0300

    Teste da aula 10 -  #funcionou

--- F1M3 JavaScript/Git/teste.js
+++ F1M3 JavaScript/Git/teste.js
@@ -1,28 +1,47 @@
 const prompt = require('prompt-sync')();
 
-for (i = 1; i <= 5; i++) {
+var nome = prompt("Digite seu nome: ");
+var salario_bruto = parseFloat(prompt("Digite seu salário bruto: "));
+var dependentes = parseInt(prompt("Digite a quantidade de dependentes: "));
+var renda_familiar = 0
 
-    var nome = prompt("Digite seu nome: ");
-    var salario_bruto = parseFloat(prompt("Digite seu salário bruto: "));
-    var dependentes = parseInt(prompt("Digite o número de dependentes: "));
-
-    var renda_per_capta = salario_bruto / (dependentes + 1);
-
-
-if (renda_per_capta >= 500) {
-
-    if (salario_bruto > 0 && salario_bruto <= 1903.86) {
-        var ir = salario_bruto * 0.05;
-    } else if (salario_bruto <= 2826.65) {
-        var ir = salario_bruto * 0.075;
-    } else {
-        var ir = salario_bruto * 0.15;
+if (dependentes > 0) {
+    for (var dep = 1; dep <= dependentes; dep++) {
+        var nome = prompt("Digite o nome do dependente: ");
+        var ganho_mensal = prompt("Digite o ganho mensal do seu dependente: ");
+        var renda_familiar = salario_bruto + ganho_mensal;
     }
-}
+    renda_familiar /= dependentes;
+    if (renda_familiar >= 500) {
+        if (salario_bruto <= 1903.98) {
+            var salario_liquido = salario_bruto * 0.95;
+            console.log("Seu salário líquido é: R$ ", salario_liquido);
+            console.log("O desconto aplicado foi de: R$ ", salario_bruto * 0.05);
+        } else if (salario_bruto > 1903.98 && salario_bruto <= 2826.65) {
+            var salario_liquido = salario_bruto * 0.925;
+            console.log("Seu salário líquido é: R$ ", salario_liquido);
+            console.log("O desconto aplicado foi de: R$ ", salario_bruto * 0.075)
+        } else {
+            var salario_liquido = salario_bruto * 0.85;
+            console.log("Seu salário líquido é: R$ ", salario_liquido);
+            console.log("O seu desconto aplicado foi de:  R$ ", salario_bruto * 0.15)
+        }
+    } else {
+        console.log("Você está inseto de imposto de renda.");
+    }
+} else if (dependentes == 0) {
 
-else {
-    var ir = 0
-}
-var salario_liquido = salario_bruto - ir;
-console.log("Salário Líquido: R$ ", salario_liquido)
+    if (salario_bruto <= 1903.98) {
+        var salario_liquido = salario_bruto * 0.95;
+        console.log("Seu salário líquido é: R$ ", salario_liquido);
+        console.log("O desconto aplicado foi de: R$ ", salario_bruto * 0.05);
+    } else if (salario_bruto > 1903.98 && salario_bruto <= 2826.65) {
+        var salario_liquido = salario_bruto * 0.925;
+        console.log("Seu salário líquido é: R$ ", salario_liquido);
+        console.log("O desconto aplicado foi de: R$ ", salario_bruto * 0.075)
+    } else {
+        var salario_liquido = salario_bruto * 0.85;
+        console.log("Seu salário líquido é: R$ ", salario_liquido);
+        console.log("O seu desconto aplicado foi de:  R$ ", salario_bruto * 0.15)
+    }
 }
