7854ca81dd58e1aa2e2be22b1b47e69d2b27e634
Jonatas Nunes <jonatasnpe@hotmail.com>
2023-10-08 19:50:50 -0300

    Execício :: Aula 10

--- F1M3 JavaScript/Aula_10_Exercicio_1.js
+++ F1M3 JavaScript/Aula_10_Exercicio_1.js
@@ -1,9 +1,19 @@
 const prompt = require('prompt-sync')();
 
 do {
-    n = parseInt(prompt("Digite um número inteiro positivo: "));
-    console.log("Os números que exitem entre 1 e", n, " são: ");
-    for ( i = 1; i < n; i++ ) {
-        console.log(i)
+    var cont = 0;
+    var n = parseInt(prompt("Digite um número inteiro positivo: "));
+    console.log("Os números primos que exitem entre 1 e", n, " são: ");
+
+    for (let i = 1; i < n - 1; i++) {
+        cont = 0
+        for (let j = 1; j <= i; j++) {
+            if (i % j == 0) {
+                cont++
+            }
+        }
+        if (cont == 2) {
+            console.log(i)
+        }
     }
 } while (n >= 0);
