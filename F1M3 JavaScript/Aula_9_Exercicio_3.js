f57055bc9540b6fb780f913886d885a09dae0552
Jonatas Nunes <jonatasnpe@hotmail.com>
2023-10-09 00:28:48 -0300

    Exercício Resolvido :: Aula 09 - Exercício 03 'While'

--- F1M3 JavaScript/Aula_9_Exercicio_3.js
+++ F1M3 JavaScript/Aula_9_Exercicio_3.js
@@ -1,10 +1,13 @@
 const prompt = require('prompt-sync')();
+var cont = "s";
 var soma = 0;
-do {
+
+while (cont == "s" || cont == "S") {
+
     var n = parseInt(prompt("Digite: "));
     var cont = prompt("Deseja continuar (s/n)? ");
     soma += n;
 
-} while (cont == "s" || cont == "S");
+} 
 
 console.log("Soma: ", soma);
