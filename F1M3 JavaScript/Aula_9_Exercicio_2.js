e62859b24f466e1c351ed6dc1ae6cc91ff32ff26
Jonatas Nunes <jonatasnpe@hotmail.com>
2023-10-08 23:47:21 -0300

    Aula 09 :: Exercício 02 Resolvido 'For'

--- F1M3 JavaScript/Aula_9_Exercicio_2.js
+++ F1M3 JavaScript/Aula_9_Exercicio_2.js
@@ -1,12 +1,9 @@
-n = 1;
+for (i = 1; n <= 100; i++) {
+	
+	if (i % 10 == 0) {
+		console.log("Múltiplo de 10: ", i);
+	} else {
+		console.log(i);
+	}
 
-while (n <= 100) {
-    
-    if (n % 10 !== 0) {
-        console.log(n)
-    } else if (n % 10 == 0) {
-        console.log("Múltiplo de 10: ", n);
-    }
-    n++;
-    
 }
