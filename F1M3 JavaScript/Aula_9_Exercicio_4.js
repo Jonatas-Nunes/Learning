898decff422b18bb090b6c15c99ce0ee811959e2
Jonatas Nunes <jonatasnpe@hotmail.com>
2023-10-10 21:53:11 -0300

    Exercício Resolvido 4 :: Aula 9 - Solução do Vídeo

--- F1M3 JavaScript/Aula_9_Exercicio_4.js
+++ F1M3 JavaScript/Aula_9_Exercicio_4.js
@@ -1,16 +1,15 @@
-const prompt = require('prompt-sync')();
+const prompt = require("promp-syn")();
+
 var maior = 0;
-var n1 = parseInt(prompt("Digite o primeiro número: "));
-maior = n1;
-console.log("O maior número por enquanto é: ", n1);
+
 
 do {
-    var n = parseInt(prompt("Digite um número: "));
-    if (n > maior) {
-        maior = n;
-        console.log("O maior número é: ", maior);
-    } else {
-        console.log("Por enquanto o maior número continua sendo: ", maior);
-    }
-} while (n1 !== 0);
-console.log("Fim do programa.");
+	var n = parseInt(prompt("Digite um número: "));
+	
+	if (n > maior) {
+		maior = n;
+	}
+
+} while (n != 0);
+
+console.log("O maior valor foi: ", maior);
