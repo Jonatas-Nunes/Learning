3bf60fd8beb1c7c041a2bc7acb820ce3bf7ced9d
Jonatas Nunes <jonatasnpe@hotmail.com>
2024-01-12 22:04:41 -0300

    Minha solução da Aula 01 - Exercício 1 DOM

--- F1M4 DOM/teste.js
+++ F1M4 DOM/teste.js
@@ -1,40 +1,11 @@
-// document.write("Olá Mundo!");
-// document.write(Date());
-// document.write(document.title);
-// document.write(document.characterSet);
+var elementos = document.getElementsByTagName("*");
+document.write("Quantidade de elementos: " +  elementos.length);
 
-var corpo = document.body;
-corpo.style.background = "Gray";
-document.title = "Novo Título";
+var todosElementos = "";
 
-//getElementsById
-var h1_brasil = document.getElementById("pais_brasil");
-h1_brasil.style.background = "Yellow";
-document.write("<p> innerText: " + h1_brasil.innerText);
-document.write("<p> innerHTML: " + h1_brasil.innerHTML);
-// alert(h1_brasil.innerHTML);
+for (i = 0; i < elementos.length; i++) {
+    const elemento = elementos[i];
+    todosElementos += "<br>" + elemento.tagName;    
+}
 
-//getElementsByTagName
-var array_h2 = document.getElementsByTagName("h2");
-var h2_es = array_h2[0];
-var h2_mg = array_h2[1];
-h2_es.style.color = "pink";
-h2_mg.style.color = "DarkRed";
-
-//getElementsByName
-var array_cidade = document.getElementsByName("cidade");
-var cidade_0 = array_cidade[0];
-cidade_0.style.borderLeftColor = "Yellow";
-
-//getElementsByClassName
-var array_cidade_mg = document.getElementsByClassName("cidade-mg");
-var cidade_mg_1 = array_cidade_mg[1];
-cidade_mg_1.style.borderLeftColor = "red";
-
-//querySelector
-var pais = document.querySelector("#pais_brasil");
-pais.style.border = "2px solid Green";
-pais.style.textAlign = "center";
-
-var cidade_es_0 = document.querySelector(".cidade-es");
-cidade_es_0.style.color = "blue";
+document.write(todosElementos);
