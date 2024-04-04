c85190a01e5e70834f7a97d15c1ef34fbb5205aa
Jonatas Nunes <jonatasnpe@hotmail.com>
2024-04-03 22:58:30 -0300

    Tarefa_Semana_2

--- F2M1 Node.js/Tarefa_Semana_2/pratica3.js
+++ F2M1 Node.js/Tarefa_Semana_2/pratica3.js
@@ -1,15 +1,15 @@
 const prompt = require('prompt-sync')();
 
-try{
+try {
 
   var num1 = Number(prompt('Entre com o primeiro número: '));
   var num2 = Number(prompt('Entre com o segundo número: '));
 
-  if(num2 === 0){
-    //LANCE UM NOVO ERRO COM O OPERADOR THROW
+  if (num2 === 0) {
+    throw new Error('Divisão por zero é impossível');
   }
 
   console.log('O resultado da divisão é : ', num1/num2);
-}catch (e){
+} catch (e) {
   console.log('Houve um erro na operação: ', e.message);
 }
--- F2M1 Node.js/Tarefa_Semana_2/pratica4.js
+++ F2M1 Node.js/Tarefa_Semana_2/pratica4.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const rawData = fs.readFileSync('dados_pessoas.json');
-const data = //USE O MÉTODO JSON.PARSE PARA CONVERTER OS DADOS DO ARQUIVO JSON
+const data = JSON.parse(rawData);//USE O MÉTODO JSON.PARSE PARA CONVERTER OS DADOS DO ARQUIVO JSON
 
 
 console.log(data);
