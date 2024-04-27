0bdec8d8e5d7e9a88a1f481e10a730ef64c333f8
Jonatas Nunes <jonatasnpe@hotmail.com>
2024-04-27 15:22:41 -0300

    Aula_9.1.js content modified and Aula_9.2.js F2M2 added

--- F2M2 Criação de Páginas com Node.js/Aula_9.2.js
+++ F2M2 Criação de Páginas com Node.js/Aula_9.2.js
@@ -5,11 +5,16 @@ const porta = 443;
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write(req.url);
+
+  const parametro = url.parse(req.url, true).query;
+  res.write('<br/>' + parametro.nome);
+  res.write('<br/>' + parametro.sobrenome);
+  res.write('<br/>' + parametro.cidade)
   res.end();
 });
 
 server.listen(porta, () => { 
     console.log('Servidor rodando');
-    const url = `http://localhost:${porta}`;
-    import('open').then(open => open.default(url));
+    const { exec } = require('child_process');
+    exec('start http://localhost:' + porta);
 });
