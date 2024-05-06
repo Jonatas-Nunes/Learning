10ed4c35d965da325298f5057660a64b1a735256
Jonatas Nunes <jonatasnpe@hotmail.com>
2024-05-06 13:57:32 -0300

    Finally darkness come

--- F2M3 Pesristência de dados com Sequelize/Aula_4.1.js
+++ F2M3 Pesristência de dados com Sequelize/Aula_4.1.js
@@ -1,13 +1,13 @@
-// importar as bibliotecas que iremos utilizar
-const {Sequelize, model, DataType} = require("sequelize");
+//importar as blibiotecas que iremos utilizar
+const {Sequelize, Model, DataTypes} = require('sequelize');
 
-//abrindo conexão com o banco
+// abrindo conexão com o banco de dados
 const sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: "empresa-sqlite-aula-4.1"
+  dialect: "sqlite",
+  storage: "empresa-aula-4.sqlite"
 });
 
 (async () => {
-    //Sincronismo
-    await sequelize.sync({force: true});
+  // Sincronismo
+  await sequelize.sync({force: true});
 })();
