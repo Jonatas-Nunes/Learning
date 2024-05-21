72be8d95caa54ad7acebbc2c66763e4c3368f9a5
Jonatas Nunes <jonatasnpe@hotmail.com>
2024-05-21 11:58:53 -0300

    Prática da Aula 13 ::  Manipulação com dados Cookies

--- F2M4 Node.js intermediário, CRUD com Sequelize, cookies e sessões/Aula 13/index.js
+++ F2M4 Node.js intermediário, CRUD com Sequelize, cookies e sessões/Aula 13/index.js
@@ -11,13 +11,68 @@ let user1 = {
     cidade: 'Anchieta'
 }
 
-app.get('/adicionar/usuario', (req, res) => {
-    res.cookie('usuarioDados', user1, user2, user3, { expire: 400000 + Date.now() });
+let user2 = {
+    nome: 'João Pedro',
+    apelido: 'Jão',
+    idade: '26',
+    cidade: 'Vitória'
+}
+
+let user3 = {
+    nome: 'Rafael',
+    apelido: 'Rafa',
+    idade: '19',
+    cidade: 'Vila Velha'
+}
+
+
+
+app.get('/adicionar/usuario1', (req, res) => {
+    res.cookie('usuarioDados1', user1, { expire: 400000 + Date.now() });
     res.send('Dados dos usuários adicionados com sucesso');
-}); 
+});
+
+app.get('/adicionar/usuario2', (req, res) => {
+    res.cookie('usuarioDados2', user2, { expire: 400000 + Date.now() });
+    res.send('Dados dos usuários adicionados com sucesso');
+});
+
+app.get('/adicionar/usuario3', (req, res) => {
+    res.cookie('usuarioDados3', user3, { expire: 400000 + Date.now() });
+    res.send('Dados dos usuários adicionados com sucesso');
+});
 
 app.get('/usuario', (req, res) => {
-    res.send(req.cookies.usuarioDados.nome);
+    const usuarios = {
+        usuario1: req.cookies.usuarioDados1,
+        usuario2: req.cookies.usuarioDados2,
+        usuario3: req.cookies.usuarioDados3
+    }
+    res.send(usuarios);
+});
+
+app.get('/usuario1', (req, res) => {
+    res.send(req.cookies.usuarioDados1);
+});
+
+app.get('/usuario1/nome', (req, res) => {
+    res.send(req.cookies.usuarioDados1.nome);
+});
+
+app.get('/usuario2', (req, res) => {
+    res.send(req.cookies.usuarioDados2);
+});
+
+app.get('/usuario2/nome', (req, res) => {
+    res.send(req.cookies.usuarioDados2.nome);
+});
+
+app.get('/usuario3', (req, res) => {
+    res.send(req.cookies.usuarioDados3);
+});
+
+app.get('/usuario3/nome', (req, res) => {
+    res.send(req.cookies.usuarioDados3.nome);
 });
 
 app.get('/', (req, res) => {
